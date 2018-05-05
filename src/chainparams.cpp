3d37ecf4b697e131225ff5bd2e0b33772a893c58
cranberriez <jakevilevac@gmail.com>
2018-05-05 00:24:29 -0700

    Changing DASH Parameters for GRV

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -77,11 +77,11 @@ public:
     CMainParams()
     {
         strNetworkID = "main";
-        consensus.nMasternodePaymentsStartBlock = 100000;       // not true, but it's ok as long as it's less then nMasternodePaymentsIncreaseBlock
-        consensus.nMasternodePaymentsIncreaseBlock = 158000;    // actual historical value
+        consensus.nMasternodePaymentsStartBlock = 0;       // not true, but it's ok as long as it's less then nMasternodePaymentsIncreaseBlock
+        consensus.nMasternodePaymentsIncreaseBlock = 0;    // actual historical value
         consensus.nMasternodePaymentsIncreasePeriod = 576 * 30; // 17280 - actual historical value
         consensus.nInstantSendKeepLock = 24;
-        consensus.nBudgetPaymentsStartBlock = 328008; // actual historical value
+        consensus.nBudgetPaymentsStartBlock = 0; // actual historical value
         consensus.nBudgetPaymentsCycleBlocks = 16616; // ~(60*24*30)/2.6, actual number of blocks per month is 200700 / 12 = 16725
         consensus.nBudgetPaymentsWindowBlocks = 100;
         consensus.nBudgetProposalEstablishingTime = 60 * 60 * 24;
